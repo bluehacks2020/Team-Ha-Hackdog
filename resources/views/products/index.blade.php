596f5327e9764d8305e4f2b1a3154d28021a5b96
Joeylene Rivera <joeylenerivera@gmail.com>
2020-02-22 20:16:48 +0800

    Update Product Listing Page

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,22 +1,112 @@
 @extends('layouts.app', ['class' => 'bg-default'])
 
 @section('content')
-<div class="header bg-gradient-primary py-7 py-lg-8">
-  <div class="container">
-    <div class="header-body text-center mt-7 mb-7">
-      <div class="row justify-content-center">
-        <div class="col-lg-5 col-md-6">
-          <h1 class="text-white">PRODUCT LISTING PAGE</h1>
+<div class="header bg-white py-7 py-lg-8">
+  <div class="container products">
+    <div class="header-body mt-5 mb-7">
+      <div class="row">
+        <div class="col-2">
+          <h3>Categories</h3>
+          <ul class="list-categories">
+            <li>
+              Health & Beauty
+            </li>
+            <li>
+              Babies & Toys
+            </li>
+            <li>
+              Groceries
+            </li>
+            <li>
+              Home & Living
+            </li>
+            <li>
+              Women's Fashion
+            </li>
+            <li>
+              Men's Fashion
+            </li>
+            <li>
+              Fashion Accessories
+            </li>
+            <li>
+              Sports & Lifestyle
+            </li>
+          </ul>
+        </div>
+        <div class="col-10">
+          <div class="row">
+            <div class="col-12">
+              <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="Search by product name, location, or by brand">
+                <div class="input-group-append">
+                  <button class="btn btn-outline-primary" type="button">Search</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-4">
+              <div class="card" style="width: 100%;">
+                <img class="card-img-top" src="https://via.placeholder.com/300">
+                <div class="card-body">
+                  <span class="float-right text-md text-bold">PHP 200.00</span>
+                  <h3 class="card-title">
+                    Product Name
+                    <br>
+                    <span class="text-sm">By Bagobo Tribe</span>
+                  </h3>
+                  <a href="#" class="btn btn-sm btn-outline-primary">
+                    <i class="ni ni-cart"></i> Add to Cart
+                  </a>
+                  <a href="/products/1" class="btn btn-sm btn-outline-secondary">
+                    Learn More
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div class="col-4">
+              <div class="card" style="width: 100%;">
+                <img class="card-img-top" src="https://via.placeholder.com/300">
+                <div class="card-body">
+                  <span class="float-right text-md text-bold">PHP 200.00</span>
+                  <h3 class="card-title">
+                    Product Name
+                    <br>
+                    <span class="text-sm">By Bagobo Tribe</span>
+                  </h3>
+                  <a href="#" class="btn btn-sm btn-outline-primary">
+                    <i class="ni ni-cart"></i> Add to Cart
+                  </a>
+                  <a href="/products/1" class="btn btn-sm btn-outline-secondary">
+                    Learn More
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div class="col-4">
+              <div class="card" style="width: 100%;">
+                <img class="card-img-top" src="https://via.placeholder.com/300">
+                <div class="card-body">
+                  <span class="float-right text-md text-bold">PHP 200.00</span>
+                  <h3 class="card-title">
+                    Product Name
+                    <br>
+                    <span class="text-sm">By Bagobo Tribe</span>
+                  </h3>
+                  <a href="#" class="btn btn-sm btn-outline-primary">
+                    <i class="ni ni-cart"></i> Add to Cart
+                  </a>
+                  <a href="/products/1" class="btn btn-sm btn-outline-secondary">
+                    Learn More
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   </div>
-  <div class="separator separator-bottom separator-skew zindex-100">
-    <svg x="0" y="0" viewBox="0 0 2560 100" preserveAspectRatio="none" version="1.1" xmlns="http://www.w3.org/2000/svg">
-      <polygon class="fill-default" points="2560 0 2560 100 0 100"></polygon>
-    </svg>
-  </div>
 </div>
-
-<div class="container mt--10 pb-5"></div>
 @endsection

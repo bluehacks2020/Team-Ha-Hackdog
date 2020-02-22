b148591f314a6b8cbfd1c82fc63d751091a2efbf
Karl T. Macadangdang <karljarren0308@gmail.com>
2020-02-23 02:44:01 +0800

    Updated form

--- resources/views/dashboard/add_product.blade.php
+++ resources/views/dashboard/add_product.blade.php
@@ -15,35 +15,65 @@
                     </div>
                     <div class="card-body">
                         @include('dashboard.partials.flash')
-                        <form action="{{ route('dashboard.products.save') }}" method="POST">
+                        <form action="{{ route('dashboard.products.save') }}" method="POST" enctype="multipart/form-data">
                             @csrf
 
-                            <div class="form-group mb-3">
-                                <label>Name:</label>
-                                <div class="form-group">
-                                    <input type="text" class="form-control form-control-alternative" name="name" required autofocus>
-                                </div>
-                            </div>
-                            <div class="form-group mb-3">
-                                <label>Description:</label>
-                                <div class="form-group">
-                                    <textarea name="description" cols="30" rows="5" class="form-control form-control-alternative" style="resize: none;" required></textarea>
-                                </div>
-                            </div>
                             <div class="row">
-                                <div class="col-sm-6">
+                                <div class="col-sm-8">
+                                    <div class="row">
+                                        <div class="col-sm-8">
+                                            <div class="form-group mb-3">
+                                                <label>Name:</label>
+                                                <div class="form-group">
+                                                    <input type="text" class="form-control form-control-alternative" name="name" required autofocus>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-sm-4">
+                                            <div class="form-group mb-3">
+                                                <label>Category:</label>
+                                                <div class="form-group">
+                                                    <input type="text" class="form-control form-control-alternative" name="category" required>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                     <div class="form-group mb-3">
-                                        <label>Unit Price:</label>
+                                        <label>Description:</label>
                                         <div class="form-group">
-                                            <input type="number" step="any" class="form-control form-control-alternative" name="unit_price" required>
+                                            <textarea name="description" cols="30" rows="5" class="form-control form-control-alternative" style="resize: none;" required></textarea>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-sm-6">
+                                            <div class="form-group mb-3">
+                                                <label>Unit Price:</label>
+                                                <div class="form-group">
+                                                    <input type="number" step="any" class="form-control form-control-alternative" name="unit_price" required>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-sm-6">
+                                            <div class="form-group mb-3">
+                                                <label>In Stock:</label>
+                                                <div class="form-group">
+                                                    <input type="number" class="form-control form-control-alternative" name="in_stock" required>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                                <div class="col-sm-6">
+                                <div class="col-sm-4">
                                     <div class="form-group mb-3">
-                                        <label>In Stock:</label>
+                                        <label>Cover Image:</label>
                                         <div class="form-group">
-                                            <input type="number" class="form-control form-control-alternative" name="in_stock" required>
+                                            <input type="file" id="cover-upload" class="form-control form-control-alternative" name="image" required>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Cover Image:</label>
+                                        <div>
+                                            <img id="cover-preview" src="" alt="No image preview available." style="width: 100%;">
                                         </div>
                                     </div>
                                 </div>
@@ -64,4 +94,18 @@
 @push('js')
     <script src="{{ asset('argon') }}/vendor/chart.js/dist/Chart.min.js"></script>
     <script src="{{ asset('argon') }}/vendor/chart.js/dist/Chart.extension.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('body').on('change', '#cover-upload', function() {
+                var fr = new FileReader();
+                var image = $(this)[0].files[0];
+
+                fr.onload = function(e) {
+                    $('#cover-preview').attr('src', e.target.result);
+                };
+
+                fr.readAsDataURL(image);
+            });
+        });
+    </script>
 @endpush

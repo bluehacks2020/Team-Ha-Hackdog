ed336a6563646504fe220db4a0724210a6914791
Joeylene Rivera <joeylenerivera@gmail.com>
2020-02-22 19:42:09 +0800

    Update footer

--- resources/views/layouts/footers/nav.blade.php
+++ resources/views/layouts/footers/nav.blade.php
@@ -5,23 +5,4 @@
             <a href="https://www.updivision.com" class="font-weight-bold ml-1" target="_blank">Updivision</a>
         </div>
     </div>
-    <div class="col-xl-6">
-        <ul class="nav nav-footer justify-content-center justify-content-xl-end">
-            <li class="nav-item">
-                <a href="https://www.creative-tim.com" class="nav-link" target="_blank">Creative Tim</a>
-            </li>
-            <li class="nav-item">
-                <a href="https://www.updivision.com" class="nav-link" target="_blank">Updivision</a>
-            </li>
-            <li class="nav-item">
-                <a href="https://www.creative-tim.com/presentation" class="nav-link" target="_blank">About Us</a>
-            </li>
-            <li class="nav-item">
-                <a href="http://blog.creative-tim.com" class="nav-link" target="_blank">Blog</a>
-            </li>
-            <li class="nav-item">
-                <a href="https://github.com/creativetimofficial/argon-dashboard/blob/master/LICENSE.md" class="nav-link" target="_blank">MIT License</a>
-            </li>
-        </ul>
-    </div>
 </div>

b7d3d4877e47a3f3426eff9098cef5cc97a68fcf
Karl T. Macadangdang <karljarren0308@gmail.com>
2020-02-22 21:28:39 +0800

    Added product-related functions

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -15,9 +15,50 @@ class DashboardController extends Controller
     {
         return view('dashboard.index');
     }
+
+    public function products()
+    {
+        return view('dashboard.products');
+    }
     
     public function test()
     {
         abort('404');
+    }
+
+    /*
+    * POST Requests
+    */
+    public function postSaveProduct(Request $request)
+    {
+        $id = $request->input('id', null);
+        $name = $request->input('name');
+        $description = $request->input('description');
+        $unit_price = $request->input('unit_price');
+        $in_stock = $request->input('in_stock');
+
+        if($id !== null) {
+            $product = Products::where('id', $id)->first();
+
+            $actions = ['update', 'updated'];
+        } else {
+            $product = new Products;
+
+            $actions = ['add', 'added'];
+        }
+
+        if($product->save()) {
+            session()->flash('flash', [
+                'status' => 'ok',
+                'message' => 'Product has been successfully ' . $action[1] . '.'
+            ]);
+        } else {
+            session()->flash('flash', [
+                'status' => 'fail',
+                'message' => 'Failed to ' . $action[0] . ' product.'
+            ]);
+        }
+
+        return redirect()->back();
     }
 }

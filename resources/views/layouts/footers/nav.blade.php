ce83b6314e2e5adf41445f740170c4cc1eae8f54
robertfederico <robertbfederic0@gmail.com>
2020-02-23 06:53:08 +0800

    Update copyright

--- resources/views/layouts/footers/nav.blade.php
+++ resources/views/layouts/footers/nav.blade.php
@@ -1,8 +1,7 @@
 <div class="row align-items-center justify-content-xl-between">
     <div class="col-xl-6">
         <div class="copyright text-center text-xl-left text-muted">
-            &copy; {{ now()->year }} <a href="https://www.creative-tim.com" class="font-weight-bold ml-1" target="_blank">Creative Tim</a> &amp;
-            <a href="https://www.updivision.com" class="font-weight-bold ml-1" target="_blank">Updivision</a>
+            &copy; {{ now()->year }} Katubo
         </div>
     </div>
 </div>

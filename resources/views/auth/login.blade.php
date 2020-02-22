a458e61ba96bc648b930ecd25b2aa99376e6826a
Karl T. Macadangdang <karljarren0308@gmail.com>
2020-02-22 21:05:51 +0800

    Updated content

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -23,12 +23,7 @@
                     <div class="card-body px-lg-5 py-lg-5">
                         <div class="text-center text-muted mb-4">
                             <small>
-                                <a href="{{ route('register') }}">{{ __('Create new account') }}</a> {{ __('OR Sign in with these credentials:') }}
-                            </small>
-                            <br>
-                            <small>
-                                {{ __('Username') }} <strong>admin@argon.com</strong>
-                                {{ __('Password') }} <strong>secret</strong>
+                                <a href="{{ route('register') }}">{{ __('Create new account') }}</a> {{ __('or Sign in with your credentials:') }}
                             </small>
                         </div>
                         <form role="form" method="POST" action="{{ route('login') }}">

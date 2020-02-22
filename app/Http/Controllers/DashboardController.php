dffc84cbef79ffe537fb3865814a10172a70ec74
Karl T. Macadangdang <karljarren0308@gmail.com>
2020-02-23 02:46:17 +0800

    Minor update

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,8 +5,11 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use Auth;
+use Storage;
 
+use App\Order;
 use App\Product;
+use App\Seller;
 
 class DashboardController extends Controller
 {
@@ -22,7 +25,7 @@ class DashboardController extends Controller
 
     public function products()
     {
-        $products = Auth::user()->asWhat->products();
+        $products = Product::where('seller_id', Auth::user()->asWhat->id)->get();
 
         return view('dashboard.products', [
           'products' => $products
@@ -32,6 +35,24 @@ class DashboardController extends Controller
     public function addProduct()
     {
         return view('dashboard.add_product');
+    }
+
+    public function orders()
+    {
+        $orders = Order::where('logistic_id', Auth::user()->asWhat->id)->get();
+
+        return view('dashboard.orders', [
+          'orders' => $orders
+        ]);
+    }
+
+    public function sellers()
+    {
+        $sellers = Seller::where('logistic_id', Auth::user()->asWhat->id)->get();
+
+        return view('dashboard.sellers', [
+          'sellers' => $sellers
+        ]);
     }
     
     public function test()
@@ -48,33 +69,45 @@ class DashboardController extends Controller
         $name = $request->input('name');
         $description = $request->input('description');
         $unit_price = $request->input('unit_price');
+        $category = $request->input('category', '');
         $in_stock = $request->input('in_stock');
 
-        if($id !== null) {
-            $product = Product::where('id', $id)->first();
+        $uploaded_image = Storage::disk('public')->putFile('products', $request->file('image'));
 
-            $actions = ['update', 'updated'];
-        } else {
-            $product = new Product;
+        if($uploaded_image !== false) {
+          if($id !== null) {
+              $product = Product::where('id', $id)->first();
 
-            $actions = ['add', 'added'];
-        }
+              $actions = ['update', 'updated'];
+          } else {
+              $product = new Product;
 
-        $product->seller_id = Auth::user()->asWhat->id;
-        $product->name = $name;
-        $product->description = $description;
-        $product->unit_price = $unit_price;
-        $product->in_stock = $in_stock;
+              $actions = ['add', 'added'];
+          }
 
-        if($product->save()) {
-            session()->flash('flash', [
-                'status' => 'ok',
-                'message' => 'Product has been successfully ' . $actions[1] . '.'
-            ]);
+          $product->seller_id = Auth::user()->asWhat->id;
+          $product->name = $name;
+          $product->description = $description;
+          $product->unit_price = $unit_price;
+          $product->in_stock = $in_stock;
+          $product->category = $category;
+          $product->cover_image = basename($uploaded_image);
+
+          if($product->save()) {
+              session()->flash('flash', [
+                  'status' => 'ok',
+                  'message' => 'Product has been successfully ' . $actions[1] . '.'
+              ]);
+          } else {
+              session()->flash('flash', [
+                  'status' => 'fail',
+                  'message' => 'Failed to ' . $actions[0] . ' product.'
+              ]);
+          }
         } else {
             session()->flash('flash', [
                 'status' => 'fail',
-                'message' => 'Failed to ' . $actions[0] . ' product.'
+                'message' => 'Failed to upload product cover image.'
             ]);
         }
 
@@ -96,6 +129,30 @@ class DashboardController extends Controller
             session()->flash('flash', [
                 'status' => 'fail',
                 'message' => 'Failed to delete product.'
+            ]);
+        }
+
+        return redirect()->back();
+    }
+
+    public function postUpdateOrderStatus(Request $request)
+    {
+        $id = $request->input('id');
+        $status = $request->input('status');
+
+        $product = Product::where('id', $id)->first();
+
+        $product->delivery_status = $status;
+
+        if($product->save()) {
+            session()->flash('flash', [
+                'status' => 'ok',
+                'message' => 'Delivery status has been successfully updated.'
+            ]);
+        } else {
+            session()->flash('flash', [
+                'status' => 'fail',
+                'message' => 'Failed to update delivery status.'
             ]);
         }
 

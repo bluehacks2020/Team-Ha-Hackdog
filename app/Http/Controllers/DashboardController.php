599ddd4c7ae6984f5377bf572774c8d472d6b060
Karl T. Macadangdang <karljarren0308@gmail.com>
2020-02-22 22:46:13 +0800

    Added product-related routes

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 
 use Auth;
 
+use App\Product;
+
 class DashboardController extends Controller
 {
     public function __construct()
@@ -26,6 +28,11 @@ class DashboardController extends Controller
           'products' => $products
         ]);
     }
+
+    public function addProduct()
+    {
+        return view('dashboard.add_product');
+    }
     
     public function test()
     {
@@ -44,24 +51,30 @@ class DashboardController extends Controller
         $in_stock = $request->input('in_stock');
 
         if($id !== null) {
-            $product = Products::where('id', $id)->first();
+            $product = Product::where('id', $id)->first();
 
             $actions = ['update', 'updated'];
         } else {
-            $product = new Products;
+            $product = new Product;
 
             $actions = ['add', 'added'];
         }
 
+        $product->seller_id = Auth::user()->asWhat->id;
+        $product->name = $name;
+        $product->description = $description;
+        $product->unit_price = $unit_price;
+        $product->in_stock = $in_stock;
+
         if($product->save()) {
             session()->flash('flash', [
                 'status' => 'ok',
-                'message' => 'Product has been successfully ' . $action[1] . '.'
+                'message' => 'Product has been successfully ' . $actions[1] . '.'
             ]);
         } else {
             session()->flash('flash', [
                 'status' => 'fail',
-                'message' => 'Failed to ' . $action[0] . ' product.'
+                'message' => 'Failed to ' . $actions[0] . ' product.'
             ]);
         }
 

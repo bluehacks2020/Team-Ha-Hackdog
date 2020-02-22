e750809ef509189fca90eed7e68805f0f8605646
Karl T. Macadangdang <karljarren0308@gmail.com>
2020-02-22 16:19:11 +0800

    Removed name column and added type column

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('type')->default('Buyer');
             $table->rememberToken();
             $table->timestamps();
         });

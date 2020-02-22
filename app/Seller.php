ebd1ec436deb5f9fca18d86acf0518a4accd19e3
Karl T. Macadangdang <karljarren0308@gmail.com>
2020-02-22 21:47:35 +0800

    Added seller-product relationship

--- app/Seller.php
+++ app/Seller.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Seller extends Model
 {
+    public function products()
+    {
+        return $this->hasMany('App\Product', 'seller_id', 'id');
+    }
+
     public function displayName()
     {
         return $this->name;

5ab5f0fdff9dd42c4f323aed2c350cbc158c0d0f
Karl T. Macadangdang <karljarren0308@gmail.com>
2020-02-22 17:45:46 +0800

    Updated routes

--- routes/web.php
+++ routes/web.php
@@ -14,16 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-
-Route::group(['middleware' => 'auth'], function () {
-	Route::resource('user', 'UserController', ['except' => ['show']]);
-	Route::get('profile', ['as' => 'profile.edit', 'uses' => 'ProfileController@edit']);
-	Route::put('profile', ['as' => 'profile.update', 'uses' => 'ProfileController@update']);
-	Route::put('profile/password', ['as' => 'profile.password', 'uses' => 'ProfileController@password']);
-});
+Route::get('/home', 'DashboardController@test')->name('home');
 
 Auth::routes();
 

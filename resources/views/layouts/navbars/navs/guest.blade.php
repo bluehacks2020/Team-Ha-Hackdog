58c83af86b71ac200d701a6e721c00a3de13a2b6
Joeylene Rivera <joeylenerivera@gmail.com>
2020-02-22 17:21:45 +0800

    Update Navbar

--- resources/views/layouts/navbars/navs/guest.blade.php
+++ resources/views/layouts/navbars/navs/guest.blade.php
@@ -1,9 +1,10 @@
-<nav class="navbar navbar-top navbar-horizontal navbar-expand-md navbar-dark">
-    <div class="container px-4">
+<nav class="navbar navbar-top navbar-horizontal navbar-expand-md navbar-dark navbar-main p-0">
+    <div class="container p-0">
         <a class="navbar-brand" href="{{ route('home') }}">
-            <img src="{{ asset('argon') }}/img/brand/white.png" />
+            <img src="{{ asset('img/logo.png') }}">
         </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-collapse-main" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-collapse-main"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbar-collapse-main">
@@ -12,11 +13,13 @@
                 <div class="row">
                     <div class="col-6 collapse-brand">
                         <a href="{{ route('home') }}">
-                            <img src="{{ asset('argon') }}/img/brand/blue.png">
+                            <img src="{{ asset('img/logo.png') }}">
                         </a>
                     </div>
                     <div class="col-6 collapse-close">
-                        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbar-collapse-main" aria-controls="sidenav-main" aria-expanded="false" aria-label="Toggle sidenav">
+                        <button type="button" class="navbar-toggler" data-toggle="collapse"
+                            data-target="#navbar-collapse-main" aria-controls="sidenav-main" aria-expanded="false"
+                            aria-label="Toggle sidenav">
                             <span></span>
                             <span></span>
                         </button>
@@ -26,30 +29,52 @@
             <!-- Navbar items -->
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item">
-                    <a class="nav-link nav-link-icon" href="{{ route('home') }}">
-                        <i class="ni ni-planet"></i>
-                        <span class="nav-link-inner--text">{{ __('Dashboard') }}</span>
+                    <a class="nav-link nav-link-icon" href="#">
+                        <span class="nav-link-inner--text">Be a Partner</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link nav-link-icon" href="{{ route('register') }}">
-                        <i class="ni ni-circle-08"></i>
-                        <span class="nav-link-inner--text">{{ __('Register') }}</span>
+                    <a class="nav-link nav-link-icon" href="#">
+                        <span class="nav-link-inner--text">Help</span>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link nav-link-icon" href="{{ route('login') }}">
-                        <i class="ni ni-key-25"></i>
-                        <span class="nav-link-inner--text">{{ __('Login') }}</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link nav-link-icon" href="{{ route('profile.edit') }}">
-                        <i class="ni ni-single-02"></i>
-                        <span class="nav-link-inner--text">{{ __('Profile') }}</span>
+                        <span class="nav-link-inner--text">Login</span>
                     </a>
                 </li>
             </ul>
         </div>
+    </div>
+</nav>
+<nav class="navbar navbar-top navbar-horizontal navbar-expand-md navbar-dark mt-3 navbar-categories">
+    <div class="col-12 p-0">
+        <ul class="list-categories text-center p-3 text-uppercase">
+            <li class="nav-item">
+                <a class="nav-link nav-link-icon" href="#">
+                    <span class="nav-link-inner--text">Fashion</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link nav-link-icon" href="#">
+                    <span class="nav-link-inner--text">Clothing</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link nav-link-icon" href="#">
+                    <span class="nav-link-inner--text">Food & Beverages</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link nav-link-icon" href="#">
+                    <span class="nav-link-inner--text">Health</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link nav-link-icon" href="#">
+                    <span class="nav-link-inner--text">Lifestyle</span>
+                </a>
+            </li>
+        </ul>
     </div>
 </nav>

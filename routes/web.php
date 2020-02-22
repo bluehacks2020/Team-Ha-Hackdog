8337d3f71a9d96803455969f2e60b863a8494135
Joeylene Rivera <joeylenerivera@gmail.com>
2020-02-22 19:27:53 +0800

    Add Product Routes

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::resource('products','ProductsController')->only(['index', 'show']);
+
 Route::get('/home', 'DashboardController@test')->name('home');
 
 Auth::routes();

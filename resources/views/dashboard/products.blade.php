724f94be752c195d3f2b01372d7be3c9c0adaada
Karl T. Macadangdang <karljarren0308@gmail.com>
2020-02-22 21:58:01 +0800

    Seller's products page

---
+++ resources/views/dashboard/products.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.dashboard')
+
+@section('content')
+    <div class="header py-7" style="background-color: #6b664d;"></div>
+    <div class="container-fluid mt--6">
+        <div class="row">
+            <div class="col-sm-12">
+                @include('dashboard.partials.flash')
+                <div class="card shadow">
+                    <div class="card-header border-0">
+                        <div class="row align-items-center">
+                            <div class="col">
+                                <h3 class="mb-0">Manage Existing Products</h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="table-responsive">
+                        <table class="table align-items-center table-flush">
+                            <thead class="thead-light">
+                                <tr>
+                                    <th scope="col">Product Name</th>
+                                    <th scope="col">Description</th>
+                                    <th scope="col">Unit Price</th>
+                                    <th scope="col">In Stock</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @if($products->count() > 0)
+                                    @foreach($products as $product)
+                                        <tr>
+                                            <td>{{ $product->name }}</td>
+                                            <td>{{ $product->description }}</td>
+                                            <td>{{ $product->unit_price }}</td>
+                                            <td>{{ $product->in_stock }}</td>
+                                        </tr>
+                                    @endforeach
+                                @else
+                                    <tr>
+                                        <td>No results found.</td>
+                                    </tr>
+                                @endif
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        @include('layouts.footers.auth')
+    </div>
+@endsection
+
+@push('js')
+    <script src="{{ asset('argon') }}/vendor/chart.js/dist/Chart.min.js"></script>
+    <script src="{{ asset('argon') }}/vendor/chart.js/dist/Chart.extension.js"></script>
+@endpush
